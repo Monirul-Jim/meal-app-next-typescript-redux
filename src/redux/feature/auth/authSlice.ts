a48fca4fa2d62282a68a2b9531f879545d648d63
Monirul Jim <monirulislamjim6640@gmail.com>
2024-11-27 21:13:45 +0600

    basic setup redux

---
+++ src/redux/feature/auth/authSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type TUser = {
+  jti: string;
+  token_type: string;
+  exp: number;
+  iat: number;
+  user_id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  is_superuser: boolean;
+  roles: string[];
+};
+
+type TAuthState = {
+  user: null | TUser;
+  token: null | string;
+};
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
+// export const useCurrentToken = (state: RootState) => state.auth.token;
+// export const useCurrentUser = (state: RootState) => state.auth.user;
